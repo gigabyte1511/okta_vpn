7783512d2d92436ad720db874988c3ad00b6ae60
Конста Бе <konstabe@Konstas-MacBook-Pro.local>
2024-12-30 15:50:52 +0200

    getUser debug

--- bot/src/app/handlers/onCallback.ts
+++ bot/src/app/handlers/onCallback.ts
@@ -154,7 +154,7 @@ export async function handleOnCallback(callbackQuery: TelegramBot.CallbackQuery)
 					let sendMessage = "<b>Список пользователей:</b>\n\n\n";
 					for (const user of userList.data.clients){
 						try{
-							const chatId = Number(user.clientName.split("-")[0]);
+							const chatId = Number(user.clientName.split("-")[0]) || 0;
 							const clientInfo = await getUser(chatId);
 
 							sendMessage += `
@@ -166,7 +166,6 @@ export async function handleOnCallback(callbackQuery: TelegramBot.CallbackQuery)
 							`
 						} catch(e){
 							console.log(user.clientName);
-							console.log(e);
 						}
 					}
 					bot.sendMessage(chatId,sendMessage,{parse_mode:'HTML'});
